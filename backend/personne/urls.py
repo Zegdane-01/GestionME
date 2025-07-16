b23a10ebf687191b6bc9085fd674b9e11663ef61
Zegdane Ahmed <ahmed.zegdane01@gmail.com>
2025-07-16 10:16:37 +0100

    16/07/2025

--- backend/personne/urls.py
+++ backend/personne/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PersonneViewSet, HierarchieView, PersonneLoginView, change_password, ImportChargePlanView
+from .views import PersonneViewSet, HierarchieView, PersonneLoginView, change_password, ImportChargePlanView, download_last_imported_file
 
 router = DefaultRouter()
 router.register(r'personnes', PersonneViewSet)
@@ -11,5 +11,6 @@ urlpatterns = [
     path('login/', PersonneLoginView.as_view(), name='login'),
     path('change-password/', change_password, name='change_password'),
     path('import-excel/', ImportChargePlanView.as_view(), name='import-excel'),
+    path('download-latest-excel/', download_last_imported_file),
     
 ]
